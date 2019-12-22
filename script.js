86202ef347929c75991a809c7247d2bc126cfa64
Carlos Hernandez <carlos@devcodify.com>
2019-12-21 23:40:01 -0700

    create timeblock index.html, create show date script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+//show the date with moment.js
+
+$(document).ready(function(){
+    var todaydisplay=   moment().format('MMMM Do YYYY');
+    $("#currentDay").text(todaydisplay);
+
+     // build row components
+ 
+});
