354ae3a15f9b7769b542e489c56acc05d4878c30
Carlos Hernandez <carlos@devcodify.com>
2019-12-23 00:10:30 -0700

    save a get value in localstorage script.js

--- script.js
+++ script.js
@@ -9,7 +9,26 @@ $(document).ready(function(){
      var todaydisplay=   moment().format('MMMM Do YYYY');
      $("#currentDay").text(todaydisplay);
 
+     $(".saveBtn").on("click",function(){
+        var activity = $(this).siblings(".description").val();
+        var time= $(this).parent().attr("id");
+        localStorage.setItem(time,activity);
+     })
+     
+     // get value from localstorage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    
+   
 
-  
+      
+    
         
     });
